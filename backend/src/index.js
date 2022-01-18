ab0b38270e3841bdf97d431e83225763b321a339
RAJVISHNU <imironmantonystark26@gmail.com>
2022-01-18 13:45:41 +0530

    sample server and DB

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("mongo db connected");
+  })
+  .catch((err) => {
+    console.log("ERROR" + err);
+  });
+
+app.get("/hello", (req, res) => {
+  res.send("hello there");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("backend server runs at" + process.env.PORT);
+});
